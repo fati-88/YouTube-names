dfc57e338a645d9db835305e14693fb0da8c8832
Fati <fatimazahraelfennych@gmail.com>
2025-08-09 08:20:36 +0100

    Add files via upload

---
+++ yn.py
@@ -0,0 +1,5 @@
+#youtube channel name generator: 
+print("hello user to the YouTube channel name generator: ")
+nick=input(f"what is your nickname?")
+about=input(f"what is your channel about?")
+print(f"you could name your channel {about} with {nick}? ")
